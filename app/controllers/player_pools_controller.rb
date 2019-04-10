9c61d9bf940eff1ef5fb310c641cd731413b5d69
Mike Bonetti <mbonettimedia@gmail.com>
2019-04-09 23:19:28 -0500

    Add ability to edit a player pool

--- app/controllers/player_pools_controller.rb
+++ app/controllers/player_pools_controller.rb
@@ -47,4 +47,33 @@ class PlayerPoolsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @pool = PlayerPool.find_by(id: params[:id])
+  end
+
+  def update
+    pool = PlayerPool.find_by(id: params[:id])
+    pool.pool_selections.destroy_all
+    pool.pool_bonus_question_answers.destroy_all
+
+    selections = params[:player_pool][:pool_selections_attributes]
+    selections.each do |key, value|
+      character_name = value[:name]
+      status_id = value[:status_id]
+      character_id = Character.find_by_name(character_name).id
+      pool.pool_selections.build(:character_id => character_id, :status_id => status_id)
+    end
+
+    bonus_questions = params[:player_pool][:pool_bonus_question_answers_attributes]
+    bonus_questions.each do |key, value|
+      question_label = value[:question]
+      question_answer = value[:answer]
+      question_id = BonusQuestion.find_by_label(question_label).id
+      pool.pool_bonus_question_answers.build(:bonus_question_id => question_id, :answer => question_answer)
+    end
+
+    pool.save!
+    redirect_to root_path
+  end
+
 end
