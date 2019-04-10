c2f36d818aa7d4c02e30391c7db30fe3a140dd83
Mike Bonetti <mbonettimedia@gmail.com>
2019-04-09 22:44:52 -0500

    Add check for if player pool exists. Change new pool button to edit if so

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :require_login
+  before_action :require_login, :pool_made?
 
   def logged_in?
     !!session[:player_id]
@@ -10,6 +10,21 @@ class ApplicationController < ActionController::Base
   def require_login
     if !logged_in?
       redirect_to new_session_path
+    end
+  end
+
+  def pool_made?
+    if logged_in?
+      current_player = Player.find_by_id(session[:player_id])
+      p current_player.player_pool
+      if !current_player.player_pool
+        @pool_available = false
+      else
+        @pool_available = true
+        @pool_id = current_player.player_pool.id
+      end
+    else
+      @pool_available = false
     end
   end
 
