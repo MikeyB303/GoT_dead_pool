b8ff5b9c2232e76ba7281b93822e2c613b1dc6fa
Mike Bonetti <mbonettimedia@gmail.com>
2019-04-12 00:04:08 -0500

    Fix scoring system

--- app/models/pool_selection.rb
+++ app/models/pool_selection.rb
@@ -6,16 +6,22 @@ class PoolSelection < ApplicationRecord
 
   def points
     selected_status_label = self.status.label
-    character_status_label = self.character.status.label
+    character = self.character
+    character_status_label = character.status.label
+    locked = character.locked?
+    points_awarded = 0
+    
 
-    if selected_status_label != character_status_label
-      points_awarded = 0
-    elsif selected_status_label == "Dead" && character_status_label == "White Walker"
+
+    if locked && (selected_status_label == character_status_label)
       points_awarded = 1
-    else
       if selected_status_label == "White Walker"
         points_awarded = 2
-      else
+      end
+    end
+
+    if locked && (selected_status_label != character_status_label)
+      if locked && (selected_status_label == "Dead" && character_status_label == "White Walker")
         points_awarded = 1
       end
     end
@@ -26,18 +32,19 @@ class PoolSelection < ApplicationRecord
   def unassigned_points
     selected_status_label = self.status.label
     character = self.character
+    character_status_label = character.status.label
     locked = character.locked?
-    if !locked && selected_status_label == "Dead"
+    unassigned_points = 0
+
+    if !locked && (selected_status_label == "Alive" || selected_status_label == "Dead")
       unassigned_points = 1
-    elsif !locked && selected_status_label == "White Walker"
+    elsif !locked && (selected_status_label == "White Walker")
       unassigned_points = 2
-      if character.status.label == "Dead"
-        unassigned_points = 1
-      end
+    elsif locked && (selected_status_label == "White Walker" && character_status_label == "Dead")
+      unassigned_points = 1
     else
       unassigned_points = 0
     end
-    
     return unassigned_points
   end
 
